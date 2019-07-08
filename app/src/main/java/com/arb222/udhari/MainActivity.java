c2fd804ce23a3ee206fc095da7c54300b8073e02
Apoorva Raj Bhadani <apoorva222g@gmail.com>
2019-07-08 14:41:03 +0530

    tabs created and fragments

---
+++ app/src/main/java/com/arb222/udhari/MainActivity.java
@@ -0,0 +1,30 @@
+package com.arb222.udhari;
+
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tabLayout = (TabLayout) findViewById(R.id.tablayout_id);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.AddFragment(new HomeFragment(), "Home");
+        adapter.AddFragment(new NotificationsFragment(), "Notifications");
+        adapter.AddFragment(new ProfileFragment(), "Profile");
+
+        viewPager.setAdapter(adapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/arb222/udhari/ProfileFragment.java
@@ -0,0 +1,22 @@
+package com.arb222.udhari;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ProfileFragment extends Fragment {
+    View view;
+    public ProfileFragment() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_profile,container,false);
+        return view;
+    }
+}
